1020777305e15f760000c976576358d9ea44be35
Cristian-Paul BARA <cpbara@umich.edu>
2020-11-02 09:47:32 -0500

    Update model_trainer.py

--- src/utils/model_trainer.py
+++ src/utils/model_trainer.py
@@ -2,7 +2,16 @@ from torch import nn
 import torch
 
 class ModelTrainer:
-    def __init__(self, model, criterion, optimizer, batch_preproc = None, batch_postproc = None, epoch_postproc = None, device = 'cpu') -> None:
+    def __init__(   self, 
+                    model, 
+                    criterion, 
+                    optimizer, 
+                    batch_preproc = None, 
+                    batch_postproc = None, 
+                    epoch_postproc = None, 
+                    device = 'cpu',
+                    max_grad_norm = 1.0
+                    ) -> None:
         self.model = model.to(device)
         self.criterion = criterion
         self.optimizer = optimizer(self.model)
@@ -19,6 +28,7 @@ class ModelTrainer:
         else:
             self.parse_epoch = epoch_postproc
         self.device = device
+        self.max_grad_norm = max_grad_norm
         
     def forward(self,batch):
         data, labels = self.prep_batch(batch)
@@ -31,7 +41,7 @@ class ModelTrainer:
         self.optimizer.zero_grad()
         loss, prediction, labels = self.forward(batch)
         loss.backward()
-        nn.utils.clip_grad_norm_(self.model.parameters(), 1.0)
+        nn.utils.clip_grad_norm_(self.model.parameters(), self.max_grad_norm)
         self.optimizer.step()
         return loss.item(), self.parse_batch(prediction, labels)
     
