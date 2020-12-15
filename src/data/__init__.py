01558b826519d64cc5d0eaa4b3dc1e1f5fdf6bc4
Cristian Paul Bara <cpbara@umich.edu>
2020-12-15 18:19:33 -0500

    code reorg

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -3,3 +3,4 @@ from src.data.data_splitters import *
 from src.data.full_data_loader import *
 from src.data.noun_grounding_data_loader import *
 from src.data.rgb_video_loader import *
+from src.data.action_data_loader import *
