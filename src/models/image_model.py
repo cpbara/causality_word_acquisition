ccfc8eab153724197f4e6807bcc20c0634d96a2f
Cristian Paul Bara <cpbara@umich.edu>
2020-12-15 18:20:11 -0500

    new inputs

--- src/models/image_model.py
+++ src/models/image_model.py
@@ -1,9 +1,9 @@
 from torch import nn
 from torchvision.models import vgg16 as VGG16
 
-class ImageModel(nn.Module):
+class ImageModel_4096(nn.Module):
     def __init__(self):
-        super(ImageModel, self).__init__()
+        super(ImageModel_4096, self).__init__()
         
         self.conv_model = nn.Sequential(
             *list(VGG16(pretrained=True).children())[:-1],
@@ -15,3 +15,14 @@ class ImageModel(nn.Module):
         
     def forward(self,x):
         return self.img_linear(self.conv_model(x).reshape(-1,512*7*7))
+
+class ImageModel_7x7x512(nn.Module):
+    def __init__(self):
+        super(ImageModel_7x7x512, self).__init__()
+        
+        self.conv_model = nn.Sequential(
+            *list(VGG16(pretrained=True).children())[:-1],
+            )
+        
+    def forward(self,x):
+        return self.conv_model(x).reshape(-1,512*7*7)
